5c4c1c1adffb99db35faac6a60b27f76bbbd0dd7
François Roland <francois.roland@heh.be>
2025-10-04 16:36:49 +0200

    Write unit tests.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# base
+# Boucles - TP2
 
-Consignes
+## Consignes
+
+Ecrire une fonction qui permet de construire une chaîne de caractère constituée de `n` lignes contenant chacunes `n` étoiles (le caractère `*`).
+Le nombre `n` est le paramètre de la fonction.
+
+## Astuce
+
+- En programmation, pour séparer les lignes, on utilise le retour à la ligne, un caractère spécial qui s'écrit `\n`.
+- Terminer la chaine de caractères par un retour à la ligne.
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,17 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {boucle2} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('La fonction boucle2 doit retourner', () => {
+    test('1 étoile', () => {
+        expect(boucle2(1)).toEqual("*\n");
+    });
+
+    test('3x3 étoiles', () => {
+        expect(boucle2(3)).toEqual("***\n***\n***\n");
+    });
+
+    test('5x5 étoiles', () => {
+        expect(boucle2(5)).toEqual("*****\n*****\n*****\n*****\n*****\n");
     });
 });
